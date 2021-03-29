1bceb817064d9e061687d4be8b41040883193677
DevianWahyu <prof.devian.wahyu@gmail.com>
2021-03-29 10:26:25 +0700

    add pre pertemuan5

---
+++ src/main/java/pertemuan5/soal1/Soal.java
@@ -0,0 +1,19 @@
+package pertemuan5.soal1;
+
+// Soal
+//    Terdapat sebuah rumah adopsi kucing. Tempat ini akan menerima kucing liar apapun
+//    yang sebatangkara agar dapat di adopsi oleh orang yang menginginkannya. Rumah
+//    tersebut memiliki sistem pemberian identitas kepada kucing yang masuk kesana
+//    berupa angka yang akan dicetak pada kalung kucing tersebut. Saat masuk ke rumah itu,
+//    kucing liar akan diidentifikasi terlebih dahulu rasnya, baru diberi nomor identitas.
+//    Nomor identitas memiliki aturan:
+//        - Kucing pertama akan mendapat angka 1000
+//        - Kucing selanjutnya akan mendapat angka 1005, 1010, 1015, dan seterusnya
+//        - Nomer identitas di generate oleh sistem, sehingga pemilik rumah adopsi tidak
+//        perlu memberikan secara manual
+//    Buatlah program untuk membantu rumah adopsi itu menentukan nomor identitas kucing
+
+public class Soal {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/pertemuan5/soal3/Soal.java
@@ -0,0 +1,17 @@
+package pertemuan5.soal3;
+
+// Soal
+//    Sebuah toko baju menjual baju berbagai merk branded. Toko tersebut menyediakan
+//    merk:
+// 	    - Denim
+// 	    - Levis
+// 	    - Jubah
+// 	    - Koko
+//    Setiap baju memiliki harga dan merk baju. Lalu toko tersebut dapat melakukan
+//    suatu aktivitas buatBanyakBaju yang menghasilkan kumpulan baju dengan merk
+//    sama namun dengan harga yang berselisih 10000. buatBanyakBaju dapat dipanggil
+//    tanpa harus membuat objek baju terlebih dahulu dan menerima inputan berupa
+//    hargaAwal, jumlahBaju, dan merkBaju
+
+public class Soal {
+}
